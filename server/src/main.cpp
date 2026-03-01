5d2d9501ba845195bb226ead9aaa0b0ac5ce849f
Sc1pex <scipex.dev@gmail.com>
2026-03-01 16:48:12 +0200

    parse messages on esp and write back from serial

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -9,11 +9,15 @@ WiFiServer server(15113);
 enum MessageKind {
     MessageKindData,
     MessageKindConfig,
+    MessageKind_Count,
 };
 
 struct MessageHeader {
     uint16_t kind;
     uint16_t size;
+
+    MessageHeader(uint16_t kind, uint16_t size) : kind(kind), size(size) {
+    }
 
     MessageHeader(uint8_t* buffer) {
         kind = ntohs(*(uint16_t*) buffer);
@@ -28,6 +32,58 @@ struct MessageHeader {
         Serial.println(" }");
     }
 };
+
+enum class ParseMessageResult {
+    Success,
+    NotEnoughData,
+    InvalidKind,
+    InvalidSize,
+};
+
+void read_all(WiFiClient& client, uint8_t* buffer, size_t size) {
+    size_t total_read = 0;
+    while (total_read < size) {
+        if (client.available()) {
+            int bytes_read = client.read(buffer + total_read, size - total_read);
+            if (bytes_read > 0) {
+                total_read += bytes_read;
+            }
+        }
+    }
+}
+
+void sendMessage(WiFiClient& client, MessageHeader header, const uint8_t* message) {
+    uint8_t header_buf[4];
+    *(uint16_t*) header_buf = htons(header.kind);
+    *(uint16_t*) (header_buf + 2) = htons(header.size);
+
+    client.write(header_buf, sizeof(header_buf));
+    client.write(message, header.size);
+}
+
+ParseMessageResult readMessage(WiFiClient& client, MessageHeader& header, uint8_t* message_buf) {
+    if (client.available() < sizeof(MessageHeader)) {
+        return ParseMessageResult::NotEnoughData;
+    }
+
+    static uint8_t header_buf[4];
+    read_all(client, header_buf, sizeof(header_buf));
+    header = MessageHeader(header_buf);
+
+    if (header.kind >= MessageKind_Count) {
+        Serial.print("Invalid message kind: ");
+        Serial.println(header.kind);
+        return ParseMessageResult::InvalidKind;
+    }
+    if (header.size > MAX_MESSAGE_SIZE) {
+        Serial.print("Invalid message size: ");
+        Serial.println(header.size);
+        return ParseMessageResult::InvalidSize;
+    }
+
+    read_all(client, message_buf, header.size);
+    return ParseMessageResult::Success;
+}
 
 void setup() {
     Serial.begin(115200);
@@ -44,43 +100,46 @@ void setup() {
     server.setNoDelay(true);
 }
 
-void read_all(WiFiClient& client, uint8_t* buffer, size_t size) {
-    size_t total_read = 0;
-    while (total_read < size) {
-        if (client.available()) {
-            int bytes_read = client.read(buffer + total_read, size - total_read);
-            if (bytes_read > 0) {
-                total_read += bytes_read;
+uint8_t message_buf[MAX_MESSAGE_SIZE];
+uint8_t serial_buf[128];
+
+WiFiClient client;
+
+void loop() {
+    if (!client || !client.connected()) {
+        client = server.accept();
+    }
+
+    MessageHeader header(0, 0);
+    if (client && client.available() >= sizeof(MessageHeader)) {
+        ParseMessageResult result = readMessage(client, header, message_buf);
+        if (result == ParseMessageResult::Success) {
+            header.debugPrint();
+        } else {
+            Serial.print("Failed to read message: ");
+            switch (result) {
+            case ParseMessageResult::NotEnoughData:
+                Serial.println("Not enough data");
+                break;
+            case ParseMessageResult::InvalidKind:
+                Serial.println("Invalid kind");
+                break;
+            case ParseMessageResult::InvalidSize:
+                Serial.println("Invalid size");
+                break;
+            default:
+                Serial.println("Unknown error");
+                break;
             }
         }
     }
-}
 
-uint8_t header_buf[4];
-uint8_t message_buf[MAX_MESSAGE_SIZE];
+    if (Serial.available()) {
+        size_t bytes_read = Serial.readBytes(serial_buf, sizeof(serial_buf));
 
-void loop() {
-    WiFiClient client = server.accept();
-
-    if (client) {
-        Serial.println("Client connected");
-        while (client.connected()) {
-            if (client.available()) {
-                read_all(client, header_buf, sizeof(header_buf));
-                MessageHeader header(header_buf);
-                header.debugPrint();
-
-                if (header.size > MAX_MESSAGE_SIZE) {
-                    Serial.println("Message size exceeds maximum, disconnecting client");
-                    break;
-                }
-
-                read_all(client, message_buf, header.size);
-                Serial.print("Received message: ");
-                Serial.write(message_buf, header.size);
-                Serial.println();
-            }
+        if (bytes_read > 0 && client && client.connected()) {
+            MessageHeader header(MessageKindData, bytes_read);
+            sendMessage(client, header, serial_buf);
         }
-        Serial.println("Client disconnected");
     }
 }
