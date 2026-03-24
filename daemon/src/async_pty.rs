88bd216742e517eaf9061e77c172755ee83a0ef3
Sc1pex <scipex.dev@gmail.com>
2026-03-24 23:59:41 +0200

    probably correct constant for macos ioctl

--- daemon/src/async_pty.rs
+++ daemon/src/async_pty.rs
@@ -37,10 +37,7 @@ const TIOCPKT_IOCTL: u8 = 0x40;
 nix::ioctl_write_ptr_bad!(tiocpkt, nix::libc::TIOCPKT, i32);
 
 #[cfg(target_os = "macos")]
-const TIOCEXTPROC: u64 = 0x80047471;
-
-#[cfg(target_os = "macos")]
-nix::ioctl_write_ptr_bad!(tiocextproc, TIOCEXTPROC, i32);
+nix::ioctl_write_ptr_bad!(tiocextproc, nix::libc::TIOCEXT, i32);
 
 impl AsyncPty {
     pub fn new(pty: PtyMaster) -> io::Result<Self> {
