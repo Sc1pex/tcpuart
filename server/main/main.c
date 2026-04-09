a7c46ab867c7b7388f1ff4337e1aecc23d6666e6
Sc1pex <scipex.dev@gmail.com>
2026-04-10 00:35:12 +0300

    small refactor

--- server/main/main.c
+++ server/main/main.c
@@ -6,19 +6,26 @@
 #include "uart.h"
 #include "wifi.h"
 
+typedef struct {
+    QueueHandle_t tcp_to_uart_queue;
+    UartTaskParams uart_params;
+    TcpTaskParams tcp_params;
+} AppState;
+
+static AppState s_state;
+
+static void state_init(AppState* state) {
+    state->tcp_to_uart_queue = xQueueCreate(16, sizeof(Message));
+
+    state->uart_params.tcp_to_uart_queue = state->tcp_to_uart_queue;
+    state->tcp_params.tcp_to_uart_queue = state->tcp_to_uart_queue;
+}
+
 void app_main(void) {
+    state_init(&s_state);
+
     wifi_init();
 
-    // Create queue for TCP → UART communication
-    QueueHandle_t tcp_to_uart_queue = xQueueCreate(16, sizeof(Message));
-
-    // Setup task parameters
-    static UartTaskParams uart_params = { 0 };
-    uart_params.tcp_to_uart_queue = tcp_to_uart_queue;
-
-    static TcpTaskParams tcp_params = { 0 };
-    tcp_params.tcp_to_uart_queue = tcp_to_uart_queue;
-
-    xTaskCreatePinnedToCore(uart_task, "uart_task", 4096, &uart_params, 5, NULL, 1);
-    xTaskCreatePinnedToCore(tcp_task, "tcp_task", 4096, &tcp_params, 5, NULL, 0);
+    xTaskCreatePinnedToCore(uart_task, "uart_task", 4096, &s_state.uart_params, 5, NULL, 1);
+    xTaskCreatePinnedToCore(tcp_task, "tcp_task", 4096, &s_state.tcp_params, 5, NULL, 0);
 }
